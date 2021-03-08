167f3c62c40717768bd6236512f291b1e196da76
Andrej <bap2585@gmail.com>
2021-03-08 22:20:15 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
-document.querySelector('.hamburger_menu').addEventListener('click', () => {
-    document.querySelector('.container').classList.toggle('change');
-});
+// document.querySelector('.hamburger_menu').addEventListener('click', () => {
+//     document.querySelector('.container').classList.toggle('change');
+// });
 
+$(function () {
+    $(".portfolio__slider").slick({
+      prevArrow:
+        '<button type="button" class="slick-prev"><img src="images/arrow-1.svg" alt=""></button>',
+      nextArrow:
+        '<button type="button" class="slick-next"><img src="images/arrow-2.svg" alt=""></button>',
+      autoplay: true,
+      fade: true,
+    });
+  });
