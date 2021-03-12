70a1d7719e39a281797edb64426a9dc3073ec15f
Andrej <bap2585@gmail.com>
2021-03-12 11:22:39 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,12 +3,56 @@
 // });
 
 $(function () {
-    $(".portfolio__slider").slick({
-      prevArrow:
-        '<button type="button" class="slick-prev"><img src="images/arrow-1.svg" alt=""></button>',
-      nextArrow:
-        '<button type="button" class="slick-next"><img src="images/arrow-2.svg" alt=""></button>',
-      autoplay: true,
-      fade: true,
-    });
+  $(".portfolio__slider").slick({
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="images/arrow-1.svg" alt=""></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="images/arrow-2.svg" alt=""></button>',
+    autoplay: true,
+    fade: true,
   });
+});
+
+const navbar = document.querySelector(".navbar");
+const navbarOffsetTop = navbar.offsetTop;
+const sections = document.querySelectorAll("section");
+const navbarLinks = document.querySelectorAll(".navbar-link");
+const progress = document.querySelector(".progress-bars-wrapper");
+const progressBarPercents = [97, 89, 85, 87, 80, 190, 50];
+
+window.addEventListener("scroll", () => {
+  mainFn();
+});
+
+const mainFn = () => {
+  if (window.pageYOffset >= navbarOffsetTop) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+
+  sections.forEach((section, i) => {
+    if (window.pageYOffset >= section.offsetTop - 10) {
+      navbarLinks.forEach((navbarLink) => {
+        navbarLink.classList.remove("change");
+      });
+      navbarLinks[i].classList.add("change");
+    }
+  });
+
+  if (window.pageYOffset + window.innerHeight >= progress.offsetTop) {
+    document.querySelectorAll(".progress-percent").forEach((el, i) => {
+      el.style.width = `${progressBarPercents[i]}%`;
+      el.previousElementSibling.firstElementChild.textContent =
+        progressBarPercents[i];
+    });
+  }
+};
+
+mainFn();
+
+window.addEventListener("resize", () => {
+  window.location.reload();
+});
+
+
