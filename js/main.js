642a0a4857b27cdc7db71b12f76a2bd35f22bc41
Andrej <bap2585@gmail.com>
2021-03-17 16:42:09 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -56,3 +56,23 @@ window.addEventListener("resize", () => {
 });
 
 
+$(document).ready(function() {
+
+	//E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+});
